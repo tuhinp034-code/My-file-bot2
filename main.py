2e930c96e72494cbbe01b5649b49647da1c4c1fc
tuhinp034-code <tuhinp034@gmail.com>
2026-06-19 12:17:18 +0530

    main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 # HARDCODE YOUR VALUES HERE
-TOKEN = 8605688893:AAFFTwi2HiyB4FKRWKLCXVhX91nfqa4bjZ4  # Paste your token here inside the quotes
-CHANNEL_ID = -1004334993882    # Paste your channel ID here (no quotes)
+TOKEN = "8605688893:AAFFTwi2HiyB4FKRWKLCXVhX91nfqa4bjZ4"
+CHANNEL_ID = -1004334993882
 
 async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = await context.bot.forward_message(
@@ -17,6 +17,8 @@ async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"File saved! Link: {link}")
 
 if __name__ == '__main__':
+    print("Bot is starting up...")
     app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(MessageHandler(filters.Document.ALL | filters.PHOTO | filters.VIDEO, handle_file))
+    app.add_handler(MessageHandler(filters.ATTACHMENT | filters.PHOTO | filters.VIDEO | filters.Document.ALL, handle_file))
+    print("Bot is now polling...")
     app.run_polling()
